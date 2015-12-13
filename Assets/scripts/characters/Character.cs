1b1e3e0e897635e469ca8fe56c32373770f15568
Josh Milstead <jmilst20@gmail.com>
2015-12-12 23:57:03 -0500

    added more animations

--- Assets/scripts/characters/Character.cs
+++ Assets/scripts/characters/Character.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class Character : MonoBehaviour {
-
+	protected Animator animator;
 	// Use this for initialization
 	public virtual void Start () {
-	
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	public virtual void Update () {
-	
+		if (Input.GetKeyDown("space")){
+			Hit();
+		} else if (Input.GetKeyDown("left shift") || Input.GetKeyDown("right shift")){
+			Block();
+		}
+	}
+
+	public virtual void Hit(){
+		if(animator != null){
+			animator.SetTrigger("hit");
+		}
+	}
+
+	public virtual void Block(){
+		if(animator != null){
+			animator.SetTrigger("block");
+		}
 	}
 }
